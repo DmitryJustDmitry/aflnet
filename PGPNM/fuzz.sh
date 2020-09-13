bae83f8ea3bc2462cb820b0eea6e0780e473f8b7
DmitryJustDmitry <46653985+DmitryJustDmitry@users.noreply.github.com>
2020-09-13 17:47:42 +0300

    Update fuzz.sh
    Initial timeouts, this works

--- PGPNM/fuzz.sh
+++ PGPNM/fuzz.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../afl-fuzz -i in -o out -t 10000 -N tcp://127.0.0.1/15432 -P PGSQL -D 700 -K -E -- ../../pgbuild/bin/postgres -D ../../pgbuild/cls0
+../afl-fuzz -i in -o out -t 100000 -N tcp://127.0.0.1/15432 -w 200000 -P PGSQL -D 300000 -K -E -- ../../pgbuild/bin/postgres -D ../../pgbuild/cls0
